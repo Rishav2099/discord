fb7c730076d624b316e7a9e47e47ff929ad8406c
Rishav <pinkyjaiswal7008@gmail.om>
2025-05-01 19:58:02 +0530

    first-commit

---
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Server id page
+    </div>
+  )
+}
+
+export default page
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'auqsgql36h.ufs.sh',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
